d6189ba9d702856444332842229e85b2f1a1072d
Lucas Druschke <ldruschk@posteo.de>
2019-11-21 20:11:31 +0100

    add support for SESSION_COOKIE_SAMESITE

--- tests/test_cookieflags.py
+++ tests/test_cookieflags.py
@@ -29,3 +29,29 @@ def test_httponly_true(app, client):
     client.get('/store-in-session/k1/value1/')
     cookie = client.get_session_cookie()
     assert cookie.has_nonstandard_attr('HttpOnly')
+
+
+def test_samesite_none(app, client):
+    app.config['SESSION_COOKIE_SAMESITE'] = None
+
+    client.get('/store-in-session/k1/value1/')
+    cookie = client.get_session_cookie()
+    assert not cookie.has_nonstandard_attr('SameSite')
+
+
+def test_samesite_lax(app, client):
+    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+
+    client.get('/store-in-session/k1/value1/')
+    cookie = client.get_session_cookie()
+    assert cookie.has_nonstandard_attr('SameSite')
+    assert cookie.get_nonstandard_attr('SameSite') == 'Lax'
+
+
+def test_samesite_strict(app, client):
+    app.config['SESSION_COOKIE_SAMESITE'] = 'Strict'
+
+    response = client.get('/store-in-session/k1/value1/')
+    cookie = client.get_session_cookie()
+    assert cookie.has_nonstandard_attr('SameSite')
+    assert cookie.get_nonstandard_attr('SameSite') == 'Strict'
